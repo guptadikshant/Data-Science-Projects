4d25896e0703370394652e09d5016f38f843e733
Dikshant Gupta <guptadikshant99@gmail.com>
2022-10-18 22:53:31 +0530

    Created folder structure for projects

--- Project-1_Sensor_Fault_Detection/setup.py
+++ Project-1_Sensor_Fault_Detection/setup.py
@@ -1,4 +1,3 @@
-from distutils.core import setup
 from setuptools import find_packages,setup
 
 
@@ -7,6 +6,7 @@ setup(
     version="0.0.1",
     author="Dikshant Gupta",
     author_email="guptadikshant99@gmail.com",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=["pymongo==4.2.0"]
 )
 
