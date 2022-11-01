2bf248a3e11d3fbdfbaee2422d999d56ccfdf8e2
Dikshant Gupta <guptadikshant99@gmail.com>
2022-11-01 23:02:11 +0530

    High level creation of training pipeline

--- Project-1_Sensor_Fault_Detection/main.py
+++ Project-1_Sensor_Fault_Detection/main.py
@@ -1,11 +1,14 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
-
 from sensor.entity.config_entitiy import TrainingPipelineConfig, DataIngestionConfig
+from sensor.pipeline.training_pipeline import TrainingPipeline
 
 if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
-    print(mongodb_client.database.list_collection_names())
+    # mongodb_client = MongoDBClient()
+    # print(mongodb_client.database.list_collection_names())
+    #
+    # training_pipeline = TrainingPipelineConfig()
+    # data_ingestion = DataIngestionConfig(training_pipeline_config=training_pipeline)
+    # print(data_ingestion.__dict__)
 
-    training_pipeline = TrainingPipelineConfig()
-    data_ingestion = DataIngestionConfig(training_pipeline_config=training_pipeline)
-    print(data_ingestion.__dict__)
+    training_pipeline = TrainingPipeline()
+    training_pipeline.run_pipeline()
