beb7ef76cd68c360b7f4481bd80b03f22ce132d6
Dikshant Gupta <guptadikshant99@gmail.com>
2022-11-08 17:24:32 +0530

    Updated logger.py to print logs in console

--- Project-1_Sensor_Fault_Detection/sensor/logger.py
+++ Project-1_Sensor_Fault_Detection/sensor/logger.py
@@ -1,7 +1,7 @@
 import logging
 import os
 from datetime import datetime
-
+import sys
 from from_root import from_root
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
@@ -13,7 +13,9 @@ os.makedirs(logs_path, exist_ok=True)
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[ %(asctime)s ] %(lineno)d  %(module)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+    # To print the logs at console. To not print in console remove handlers
+    handlers=[logging.StreamHandler(sys.stdout),
+              logging.FileHandler(LOG_FILE_PATH)]
 )
